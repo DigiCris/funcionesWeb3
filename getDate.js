82a4ecee4822653d3c51f8b347bb56152e18210c
cmarchese <cmarchesetdiii@gmail.com>
2023-09-30 08:15:54 -0300

    saque el date para afuera de web3 y deje que web3 lo devuelva como epoch

--- getDate.js
+++ getDate.js
@@ -17,10 +17,10 @@ async function getTransactionDate(transactionHash) {
     const block = await web3.eth.getBlock(transaction.blockHash);
 
     // Convierte bigint a string para que no haya problemas
-    var timestamp = block.timestamp.toString(); s
+    var timestamp = block.timestamp.toString();
     timestamp = timestamp * 1000;// Multiplica por 1000 para convertir a milisegundo y cambiarlo a int
     const date = new Date(timestamp); // Lo ponemos en formato legible
-    return date; // Devolvemos el valor
+    return timestamp; // Devolvemos el valor
 
   } catch (error) {
     console.error('Error al obtener la fecha y hora de la transacción:', error);
@@ -28,7 +28,11 @@ async function getTransactionDate(transactionHash) {
 }
 
 // Ejemplo de uso:
-const transactionHash = '0xe26518fb361657ca96d090952042d265f17301f3ad8b6bd9e51b8ef6a343e206';
-getTransactionDate(transactionHash)
-  .then(result => console.log('res:', result))
-  .catch(error => console.error(error));
+async function main() {
+  const transactionHash = '0xe26518fb361657ca96d090952042d265f17301f3ad8b6bd9e51b8ef6a343e206';
+  var res = await getTransactionDate(transactionHash);
+  const date = new Date(res); // Lo ponemos en formato legible
+  console.log(date); // Devolvemos el valor 
+}
+
+main();
